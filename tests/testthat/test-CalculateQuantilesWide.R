927ee5f19331accf4a81b4e274eafbf6beb628d8
ddtam <derek.tam.94@gmail.com>
2020-11-18 21:13:02 -0800

    split tests into classes and improved their documentation

--- tests/testthat/test-CalculateQuantilesWide.R
+++ tests/testthat/test-CalculateQuantilesWide.R
@@ -1,5 +1,11 @@
-test_that("Test basic functionality", {
- expect_equal(CalculateQuantilesWide(vec, 2), ans)
- expect_equal(CalculateQuantilesWide(vec_w_na, 2), ans)
- expect_error(CalculateQuantilesWide(vec, "2"), "ntiles must be class numeric; input ntiles has class character")
+test_that("Basic functionality", {
+  # test expected output
+  expect_equal(CalculateQuantilesWide(vec, 2), ans)
+  # test handling of NA values
+  expect_equal(CalculateQuantilesWide(vec_w_na, 2), ans)
+})
+
+test_that("Error handling", {
+  # throws error when `ntiles` is not numeric
+  expect_error(CalculateQuantilesWide(vec, "2"), "ntiles must be class numeric; input ntiles has class character")
 })
